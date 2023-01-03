ddd039dfc52b55271e39b9ba7cec9e2c08175bd0
kedrakk <keaindrak@gmail.com>
2023-01-03 21:30:23 +0630

    theme

---
+++ DesignTest/app/src/main/java/com/app/designtest/ui/theme/Theme.kt
@@ -0,0 +1,41 @@
+package com.app.designtest.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+val yellow = Color(0xFFF4CE14)
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = yellow,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+@Composable
+fun DesignTestTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
